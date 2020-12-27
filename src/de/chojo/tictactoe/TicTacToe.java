de31f7b6c76af9b5a1ff8f5a0ec43a82d2036eb4
Florian Fülling <mail@chojo.de>
2020-12-27 18:40:52 +0100

    add statistics

--- src/de/chojo/tictactoe/TicTacToe.java
+++ src/de/chojo/tictactoe/TicTacToe.java
@@ -3,18 +3,31 @@ package de.chojo.tictactoe;
 import de.chojo.tictactoe.gameboard.AbstractGameboard;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class TicTacToe {
+    private static int draws = 0;
+    private static int xWins = 0;
+    private static int oWins = 0;
+    private static long totalTurns = 0;
+    private static Map<Integer, Integer> winTurn = new HashMap<>();
+
     public static void main(String[] args) {
         AbstractGameboard gameboard = new ExampleGameboard(3, 3, 3);
-        gameboard.enablePrint(true);
+        gameboard.enablePrint(false);
         gameboard.playTillEnd(false);
-        for (int i = 0; i < 1; i++) {
-            runGame(gameboard);
+        int games = 100000;
+        for (int i = 0; i < games; i++) {
+            runGame(gameboard, i + 1);
         }
+
+        System.out.printf("Draw:  %d%nX Win: %d%nO Win: %d%nTurns: %d%n", draws, xWins, oWins, totalTurns);
+        String turnWins = winTurn.entrySet().stream().map(e -> String.format("Turn %d : %d Wins", e.getKey(), e.getValue())).collect(Collectors.joining("\n"));
+        System.out.println("Turn Wins:\n" + turnWins);
     }
 
     /**
@@ -22,14 +35,22 @@ public class TicTacToe {
      *
      * @param gameboard the gameboard where a game should be run.
      */
-    public static void runGame(AbstractGameboard gameboard) {
+    public static void runGame(AbstractGameboard gameboard, int game) {
         gameboard.reset();
         gameboard.run(getRandomSteps(gameboard));
         if (gameboard.getWinTurn() == -1) {
-            System.out.println("Draw!");
+            System.out.printf("Game %d | Draw!%n", game);
+            draws++;
         } else {
-            System.out.println(Character.toString(gameboard.getWinner()) + " wins in turn " + gameboard.getWinTurn());
+            System.out.printf("Game %d | %s wins in turn %d.%n", game, Character.toString(gameboard.getWinner()), gameboard.getWinTurn());
+            if (gameboard.getWinner() == 'X') {
+                xWins++;
+            } else {
+                oWins++;
+            }
+            winTurn.compute(gameboard.getWinTurn(), (k, v) -> v == null ? 1 : v + 1);
         }
+        totalTurns += gameboard.getCurrTurn();
     }
 
     public static int[] getRandomSteps(AbstractGameboard gameboard) {
